59fc70e68b014816df78f4b7ae9b6d0968178b85
beichen <1621395876@qq.com>
2023-04-11 21:23:14 +0800

    first commit

---
+++ src/com/learn/function.java
@@ -0,0 +1,11 @@
+package com.learn;
+
+public class function {
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public static int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ src/com/learn/test.java
@@ -0,0 +1,19 @@
+package com.learn;
+
+public class test {
+    public static void main(String[] args) {
+            function func = new function();
+            System.out.println(func.add(11,22));
+        System.out.println();
+        System.out.println(function.sub(22,11));
+        System.out.println("dsadsadasdasdadasddasdadasd" +
+                "dsadadsadsad" +
+                "sdasdsadas" +
+                "dasdasdda" +
+                "dad");
+        int da=2;
+        System.out.println(da);
+
+    }
+}
+
